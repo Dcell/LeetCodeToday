5fc9775b3992b57d46490778c8cfdc677aa5da43
Dcell <765736477@qq.com>
2020-12-13 23:23:51 +0800

    feat: 二维数组反转

--- LeetcodeQA/LeetcodeQATests/LeetcodeQATests.swift
+++ LeetcodeQA/LeetcodeQATests/LeetcodeQATests.swift
@@ -989,6 +989,62 @@ class LeetcodeQATests: XCTestCase {
         
     }
     
+//    48. 旋转图像
+//    给定一个 n × n 的二维矩阵表示一个图像。
+//
+//    将图像顺时针旋转 90 度。
+//
+//    说明：
+//
+//    你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+//
+//    示例 1:
+//
+//    给定 matrix =
+//    [
+//      [1,2,3],
+//      [4,5,6],
+//      [7,8,9]
+//    ],
+//
+//    原地旋转输入矩阵，使其变为:
+//    [
+//      [7,4,1],
+//      [8,5,2],
+//      [9,6,3]
+//    ]
+    
+    func testrotate() {
+        func rotate(_ matrix: inout [[Int]]) {
+            let size = matrix.count
+            //先对角线反转
+            for i in 0..<size {
+                for j in 0..<i {
+                    let tem = matrix[j][i]
+                    matrix[j][i] = matrix[i][j]
+                    matrix[i][j] = tem
+                }
+            }
+            
+            //再每行反转
+            for i in 0..<size {
+                for j in 0..<size/2 {
+                    let tem = matrix[i][size - j - 1]
+                    matrix[i][size - j - 1] = matrix[i][j]
+                    matrix[i][j] = tem
+                }
+            }
+        }
+        var demo = [
+            [1,2,3],
+            [4,5,6],
+            [7,8,9]
+          ]
+        
+        rotate(&demo)
+    }
+    
+    
 
     
     func testlengthOfLongestSubstring(){
