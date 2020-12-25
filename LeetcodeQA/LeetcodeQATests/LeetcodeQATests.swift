093491dc01a98b044716e3de26243e92a9ac2281
Dcell <765736477@qq.com>
2020-12-25 23:41:44 +0800

    feat：2叉树 中序

--- LeetcodeQA/LeetcodeQATests/LeetcodeQATests.swift
+++ LeetcodeQA/LeetcodeQATests/LeetcodeQATests.swift
@@ -1595,8 +1595,39 @@ class LeetcodeQATests: XCTestCase {
         print(ans2)
     }
 
-
     
+    public class TreeNode {
+         public var val: Int
+         public var left: TreeNode?
+         public var right: TreeNode?
+         public init() { self.val = 0; self.left = nil; self.right = nil; }
+         public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+         public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+             self.val = val
+             self.left = left
+             self.right = right
+         }
+     }
+    /**
+     94. 二叉树的中序遍历
+     给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+     */
+    func testinorderTraversal(){
+        func inorderTraversal(_ root: TreeNode?) -> [Int] {
+            var ans:[Int] = []
+            func dfs(_ node:TreeNode?){
+                guard let node = node else {
+                    return
+                }
+                dfs(node.left)
+                ans.append(node.val)
+                dfs(node.right)
+            }
+            dfs(root)
+            return ans
+        }
+    }
+
 
 
 }
