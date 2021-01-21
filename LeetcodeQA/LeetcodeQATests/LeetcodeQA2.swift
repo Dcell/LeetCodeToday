80d62d72024cc15d2e0705b012a90854d1760f8a
Dcell <765736477@qq.com>
2021-01-21 22:51:43 +0800

    feat: leetcode today

--- LeetcodeQA/LeetcodeQATests/LeetcodeQA2.swift
+++ LeetcodeQA/LeetcodeQATests/LeetcodeQA2.swift
@@ -1062,8 +1062,46 @@ class LeetcodeQA2: XCTestCase {
     }
 
 
-    
+    /**
+     338. 比特位计数
+     给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
 
+     示例 1:
+
+     输入: 2
+     输出: [0,1,1]
+     示例 2:
+
+     输入: 5
+     输出: [0,1,1,2,1,2]
+     */
+    
+    func testcountBits(){
+//        偶数n 1个数 等于 n/2 个数
+        // 奇数 等 n-1 1个数+ 1
+        func countBits(_ num: Int) -> [Int] {
+            var ans = Array(repeating: 0, count: num + 1)
+            for i in 0...num {
+                if i == 0 {
+                    ans[i] = 0
+                    continue
+                }
+                if i == 1 {
+                    ans[i] = 1
+                    continue
+                }
+                if i % 2 == 0 {
+                    ans[i] = ans[i/2]
+                }else{
+                    ans[i] = ans[i - 1] + 1
+                }
+            }
+            
+            return ans
+        }
+        
+        countBits(3)
+    }
 
 
     
