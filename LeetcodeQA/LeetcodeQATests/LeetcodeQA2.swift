19d13ff4ebf5aff8c291a4c1122d7a9a5ff9d78c
Dcell <765736477@qq.com>
2021-01-23 00:22:49 +0800

    leetcode today

--- LeetcodeQA/LeetcodeQATests/LeetcodeQA2.swift
+++ LeetcodeQA/LeetcodeQATests/LeetcodeQA2.swift
@@ -1145,7 +1145,72 @@ class LeetcodeQA2: XCTestCase {
         }
         topKFrequent( [1,1,1,2,2,3], 2)
     }
+    
+    
+    /**
+     394. 字符串解码
+     给定一个经过编码的字符串，返回它解码后的字符串。
 
+     编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+
+     你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+
+     此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+      
+
+     示例 1：
+
+     输入：s = "3[a]2[bc]"
+     输出："aaabcbc"
+     示例 2：
+
+     输入：s = "3[a2[c]]"
+     输出："accaccacc"
+     */
+    func testdecodeString(){
+        func decodeString(_ s: String) -> String {
+            var stack:[Character] = []
+            let chars = Array(s)
+            for i in 0..<chars.count {
+                if chars[i] == "]" {
+                    var num = ""
+                    var world = ""
+                    var worldEnd = false
+                    while !stack.isEmpty {
+                        let last = stack.removeLast()
+                        
+                        
+                        if last == "[" && !worldEnd {
+                            worldEnd = true
+                            continue
+                        }
+                        if !worldEnd {
+                            world = String(last) + world
+                        }else{
+                            if let _ = Int(String(last)){
+                                num = String(last) + num
+                            }else{
+                                stack.append(last)
+                                break
+                            }
+                        }
+                    }
+                    for _ in 0..<(Int(num) ?? 1) {
+                        stack.append(contentsOf: world)
+                    }
+                }else{
+                    stack.append(chars[i])
+                }
+            }
+            
+            return String(stack)
+        }
+        
+//        let ans = decodeString("3[a]2[bc]")
+        let ans2 = decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")
+        print("")
+    }
     
 
 
