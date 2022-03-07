1fd6be255e4964fe200e6bffba14fd524883678f
dingqili <ding_qili@dahuatech.com>
2022-03-07 10:05:27 +0800

    Create 504.七进制数.swift

---
+++ LeetcodeQA/504.七进制数.swift
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=504 lang=swift
+ *
+ * [504] 七进制数
+ */
+
+// @lc code=start
+class Solution {
+    func convertToBase7(_ num: Int) -> String {
+        return String(num, radix: 7)
+    }
+}
+// @lc code=end
+
